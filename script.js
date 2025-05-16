e93cff30e6a67d906d1a57ef8cf840c88b89e556
Krisha-Kapadia <130428693+Krisha-Kapadia@users.noreply.github.com>
2025-05-16 13:08:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const searchBox = document.getElementById("searchBox");
+const searchBtn = document.getElementById("searchBtn");
+const input = searchBox.querySelector(".input");
+
+searchBtn.addEventListener("click", () => {
+  searchBox.classList.toggle("active");
+  input.focus();
+});
